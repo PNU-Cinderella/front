afadcd5da35a8e40a63a4657e3249c0d14793205
조정현 <jojeonghyeon@jojeonghyeon-ui-MacBookAir.local>
2023-03-26 14:36:13 +0900

    Test3 Second

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_cinderella_test3/Login.dart';
+import 'package:project_cinderella_test3/Login%20copy.dart';
+// import 'package:project_cinderella_test3/Login.dart';
 import 'Tests.dart';
 
 void main() {
